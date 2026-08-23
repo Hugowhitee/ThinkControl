7791330e2f9eab8e0f76f5bde94690f6c58f5979
Hugo <119423227+Hugowhitee@users.noreply.github.com>
2026-08-23 19:16:56 +0200

    Add refined ThinkControl wordmark code-behind

---
+++ src/ThinkControl.UI/Controls/BrandWordmark.xaml.cs
@@ -0,0 +1,9 @@
+namespace ThinkControl.UI.Controls;
+
+public partial class BrandWordmark : System.Windows.Controls.UserControl
+{
+    public BrandWordmark()
+    {
+        InitializeComponent();
+    }
+}
