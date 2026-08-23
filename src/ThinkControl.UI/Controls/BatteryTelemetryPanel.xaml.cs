fe49957efe0c2d0a0371182e2f557a9c902fd8af
Hugo <119423227+Hugowhitee@users.noreply.github.com>
2026-08-23 10:13:41 +0200

    Disambiguate WPF battery user control

--- src/ThinkControl.UI/Controls/BatteryTelemetryPanel.xaml.cs
+++ src/ThinkControl.UI/Controls/BatteryTelemetryPanel.xaml.cs
@@ -1,8 +1,6 @@
-using System.Windows.Controls;
-
 namespace ThinkControl.UI.Controls;
 
-public partial class BatteryTelemetryPanel : UserControl
+public partial class BatteryTelemetryPanel : System.Windows.Controls.UserControl
 {
     public BatteryTelemetryPanel()
     {
