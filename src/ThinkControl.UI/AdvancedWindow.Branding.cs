0616e262545da70fa857045c0f8e5ba83e5b46e6
Hugo <119423227+Hugowhitee@users.noreply.github.com>
2026-08-23 23:05:19 +0200

    Fix compact view glyph import

--- src/ThinkControl.UI/AdvancedWindow.Branding.cs
+++ src/ThinkControl.UI/AdvancedWindow.Branding.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using ThinkControl.UI.Controls;
 
 namespace ThinkControl.UI;
