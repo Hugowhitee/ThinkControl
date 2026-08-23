b7499b30a1163a7bf786a68e725331be16321241
Hugo <119423227+Hugowhitee@users.noreply.github.com>
2026-08-23 13:25:22 +0200

    Add ThinkControl brand mark control code

---
+++ src/ThinkControl.UI/Controls/BrandMark.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ThinkControl.UI.Controls;
+
+public partial class BrandMark : UserControl
+{
+    public BrandMark()
+    {
+        InitializeComponent();
+    }
+}
